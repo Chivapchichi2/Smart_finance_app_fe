20e80ffec72aaad7b38bee99c0ec1ebe93d7927c
Vlad Safin <djutsu17@gmail.com>
2021-10-04 18:29:21 +0300

    added some stuff

--- src/views/homeView/homeView.js
+++ src/views/homeView/homeView.js
@@ -1,13 +1,19 @@
 import MainHome from '../../components/container/homeContainer/mainHome';
 import BoxHome from '../../components/container/homeContainer/boxHome';
-import HomeNav from '../../components/container/homeContainer/homeNav';
 import BalanceWrapper from '../../components/balanceWrapper/balanceWrapper';
+
+// import HomeNav from '../../components/container/homeContainer/homeNav';
+
+// import Notification from '../../components/notification';
 
 const HomeView = () => (
   <MainHome>
     <BoxHome />
     <BalanceWrapper />
-    <HomeNav />
+    {/* <HomeNav /> */}
+
+    {/* Окошко нотификации  */}
+    {/* <Notification /> */}
   </MainHome>
 );
 
