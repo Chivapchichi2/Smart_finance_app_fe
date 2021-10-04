011b65c95e0fd085be667202cb1b5c1a9b20b09e
KalVasFlame <unnameddwar@gmail.com>
2021-10-04 18:54:51 +0300

    add header

--- src/components/header/userBar/userBar.js
+++ src/components/header/userBar/userBar.js
@@ -1,15 +1,18 @@
 import { useWindowWidth } from '@react-hook/window-size';
 
-import { ReactComponent as LogOutBtn } from './logout.svg';
-import { UserBarContainer } from './userBarStyles';
+import { ReactComponent as LogOutBtnImg } from './logout.svg';
+import styles from './userBar.module.css';
+
 function UserBar() {
   const width = useWindowWidth();
   return (
-    <UserBarContainer>
+    <div className={styles.container}>
       <p>UserName</p>
-      {width > 767 ? <div /> : ''}
-      <button type="button">{width > 767 ? 'Выйти' : <LogOutBtn />}</button>
-    </UserBarContainer>
+      {width > 767 ? <div className={styles.vector} /> : ''}
+      <button type="button" className={styles.logOutBtnSvg}>
+        {width > 767 ? 'Выйти' : <LogOutBtnImg />}
+      </button>
+    </div>
   );
 }
 
--- src/components/header/userBar/userBarStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const UserBarContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-export { UserBarContainer };
