a520bf48fb376410e869d3193a16282fcc043a14
Vlad Safin <djutsu17@gmail.com>
2021-10-05 22:25:30 +0300

    added reports categories

--- src/views/homeView/homeView.js
+++ src/views/homeView/homeView.js
@@ -2,33 +2,35 @@
 
 import { MainHome, BoxHome } from '../../components';
 import BalanceWrapper from '../../components/balanceWrapper/balanceWrapper';
-import GoToReports from '../../components/balanceWrapper/goToReports';
-import BackToMain from '../../components/balanceWrapper/backToMain';
-import Balance from '../../components/balanceWrapper/balance';
-import CurrentPeriod from '../../components/balanceWrapper/currentPeriod';
+// import GoToReports from '../../components/balanceWrapper/goToReports';
+// import BackToMain from '../../components/balanceWrapper/backToMain';
+// import Balance from '../../components/balanceWrapper/balance';
+// import CurrentPeriod from '../../components/balanceWrapper/currentPeriod';
 // import routes from '../../routes/routes';
 // import HomeNav from '../../components/container/homeContainer/homeNav';
 // import Notification from '../../components/notification';
-// import ReportsSlider from '../../components/reportsSlider';
+import ReportsSlider from '../../components/reportsSlider';
 
 const HomeView = () => (
   <MainHome>
     <BoxHome />
-    <BalanceWrapper>
-      {/* <HomeNav /> */}
-      {/* Окошко нотификации  */}
-      {/* <Notification /> */}
-      {/* <ReportsSlider /> */}
-      <GoToReports />
+    {/* <BalanceWrapper> */}
+    {/* <HomeNav /> */}
+    {/* Окошко нотификации  */}
+    {/* <Notification /> */}
+
+    {/* <GoToReports />
       <BackToMain />
       <Balance />
-      <CurrentPeriod />
+      <CurrentPeriod /> */}
+
     {/* <Switch>
       <Route path={routes.homeExpenses} component={ExpensesView} />
       <Route path={routes.homeIncomes} component={IncomesView} />
       <Redirect to={routes.homeExpenses} />
     </Switch> */}
-    </BalanceWrapper>
+
+    <ReportsSlider />
   </MainHome>
 );
 
