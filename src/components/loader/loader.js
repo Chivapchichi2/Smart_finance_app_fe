cd7e918111a9b8026689f83061428b72702c91af
Vlad Safin <djutsu17@gmail.com>
2021-10-11 21:59:55 +0300

    added suspense lazy

---
+++ src/components/loader/loader.js
@@ -0,0 +1,13 @@
+// import Loader from 'react-loader-spinner';
+
+import Loader from 'react-js-loader';
+
+const Preloader = () => (
+  // <Loader type="TailSpin" color="#00BFFF" height={80} width={80} />
+
+  // <Loader type="spinner-cub" bgColor="#FF751D" title="spinner-cub" size={100} />
+
+  <Loader type="bubble-ping" bgColor="#FF751D" title="bubble-ping" size={200} />
+);
+
+export default Preloader;
