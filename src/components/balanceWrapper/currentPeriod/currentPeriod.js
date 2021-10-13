d1ed84e98861e0e8d3c3df806a2ff0af788a91a3
Ruslan Golovach <ruslan.golovach@ukr.net>
2021-10-13 19:30:52 +0300

    added logic current period

--- src/components/balanceWrapper/currentPeriod/currentPeriod.js
+++ src/components/balanceWrapper/currentPeriod/currentPeriod.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import { ReactComponent as IconArrowLeft } from '../../../svg/arrowLeft.svg';
 import { ReactComponent as IconArrowRight } from '../../../svg/arrowRight.svg';
 import styles from './currentPeriod.module.css';
 
 const CurrentPeriod = () => {
-  const date = new Date();
+  const [date, setDate] = useState(new Date());
+
+  const changeMonth = action => {
+    const value = action === 'prev' ? -1 : 1;
+    setDate(prevDate => {
+      const newDate = new Date(prevDate);
+      const month = newDate.getMonth();
+      newDate.setMonth(month + value);
+      if (newDate > new Date()) {
+        return prevDate;
+      }
+      return newDate;
+    });
+  };
+
+  const getMonthAndYear = date.toLocaleDateString();
+  console.log(getMonthAndYear);
+
   const year = date.getFullYear();
   const month = date.toLocaleDateString('ru', { month: 'long' });
 
@@ -11,13 +29,21 @@ const CurrentPeriod = () => {
     <div className={styles.wrapCurrentPeriod}>
       <p className={styles.textCurrentPeriod}>Текущий период:</p>
       <div className={styles.btnWrapperArrow}>
-        <button className={styles.btn} type="button">
+        <button
+          className={styles.btn}
+          type="button"
+          onClick={() => changeMonth('prev')}
+        >
           <IconArrowLeft />
         </button>
         <p className={styles.text}>
           {month} {year}
         </p>
-        <button className={styles.btn} type="button">
+        <button
+          className={styles.btn}
+          type="button"
+          onClick={() => changeMonth('next')}
+        >
           <IconArrowRight />
         </button>
       </div>
