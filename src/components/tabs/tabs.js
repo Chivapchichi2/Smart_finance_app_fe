520c5a08b2102bda75ab92ca51a50386da319a4b
VVariychuk <v.variychuk.90@gmail.com>
2021-10-18 22:45:43 +0300

    fixed tabs

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -40,14 +40,7 @@ const CustomTabs = props => {
   const [expenses, setExpenses] = useState([]);
   const [incomes, setIncomes] = useState([]);
 
-  console.log(incomesByYear);
-  console.log(expensesByYear);
-
-  useEffect(() => {}, []);
-
   useEffect(() => {
-    console.log('currentYearDataInc', currentYearDataInc);
-    console.log('currentYearDataExp', currentYearDataExp);
     if (currentYearDataExp || currentYearDataInc) {
       const exp = currentYearDataExp.map(
         i => `${monthData[+i - 1]} ${expensesByYear[parsedYear][i]}`,
@@ -58,12 +51,6 @@ const CustomTabs = props => {
       );
       setIncomes(inc);
     }
-    // if (currentYearData) {
-    //   const inc = currentYearData.map(
-    //     i => `${monthData[+i - 1]} ${incomesByYear[parsedYear][i]}`,
-    //   );
-    //   setIncomes(inc);
-    // }
   }, [incomesByYear, expensesByYear, parsedYear]);
 
   console.log('incomes', incomes);
